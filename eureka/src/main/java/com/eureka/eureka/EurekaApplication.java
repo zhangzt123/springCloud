9ef62eea264308255cb8dcae63cc3e979c69155f
zhangzt <1>
2018-11-26 16:09:58 +0800

    初始导入

---
+++ eureka/src/main/java/com/eureka/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/*springcloud注册中心*/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
