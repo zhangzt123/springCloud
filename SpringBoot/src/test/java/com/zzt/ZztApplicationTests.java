f50ca70471f3635d77a350e77c8d36904bf08f13
zhangzt <1>
2018-12-24 21:21:50 +0800

    初始导入

---
+++ SpringBoot/src/test/java/com/zzt/ZztApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zzt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZztApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
